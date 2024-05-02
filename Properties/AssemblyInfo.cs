5c8045b6a06e2f19a2e36f18972ea72863995a55
wesoree <75049633+wesoree@users.noreply.github.com>
2024-05-02 12:46:10 -0400

    update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("encapsulation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("WesEgg Interactive")]
+[assembly: AssemblyCompany("wesoree")]
 [assembly: AssemblyProduct("encapsulation")]
-[assembly: AssemblyCopyright("Copyright © WesEgg Interactive 2024")]
+[assembly: AssemblyCopyright("Copyright © wesoree 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
